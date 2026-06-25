727ae1e255303b6b27fc99f39eb3f2acbc2ad75d
DylanNguyen79 <ahuyadug@gmail.com>
2026-06-25 16:58:32 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,6 @@ function App() {
           type="text"
           placeholder="Please enter your name"
           size="30"
-          width="500 px"
           onChange={(e) => setInput(e.target.value)}
         />
         <button onClick={() => setName(input)}>Submit</button>
