cc465881497deea14f64905cf9825b22337daec9
DylanNguyen79 <ahuyadug@gmail.com>
2026-06-25 16:22:00 +0700

    first-gift-app

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+
+const gifts = [
+  "iPhone 17 Pro Max",
+  "Macbook Pro M4",
+  "Apple Vision",
+  "IPad Pro M4",
+  "Airpods Pro 4th Gen",
+];
+
+function App() {
+  const [gift, setGift] = useState("");
+  const [name, setName] = useState("");
+  const [input, setInput] = useState("");
+
+  const randomGift = () => {
+    const index = Math.floor(Math.random() * gifts.length);
+
+    setGift(gifts[index]);
+  };
+  <a href="#">Get a random gift</a>;
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Please enter your name"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={() => setName(input)}>Submit</button>
+        <h1>Welcome {name}!</h1>
+        <h1>{"Please click the button!"} </h1>
+        <h2>🎁{gift}🎁 </h2>
+        <button onClick={randomGift}>Get Reward!</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
